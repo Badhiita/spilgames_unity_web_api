a29f1f5dc9367c88ea1e17c42f03fed15a31d69b
Richard Elms <richardelms@gmail.com>
2015-03-18 13:12:54 +0100

    Version 0.0.3

--- SpilWebAPIExampleProject/Assets/Plugins/SpilGamesWebAPI/Scripts/SpilGamesAPI.cs
+++ SpilWebAPIExampleProject/Assets/Plugins/SpilGamesWebAPI/Scripts/SpilGamesAPI.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class SpilGamesAPI : MonoBehaviour 
 {
 
+
 	//debug text for example project, feel free to take this out
 	public Text debugText;
 
@@ -23,6 +24,8 @@ public class SpilGamesAPI : MonoBehaviour
 
 	//have we recived the logo texture from the portal
 	private bool _hasTexture = false;
+
+	string version = "0.0.3";
 	
 	void Start () 
 	{
@@ -32,6 +35,7 @@ public class SpilGamesAPI : MonoBehaviour
 			"(function() {function onLoadError () {if(typeof u !== 'undefined' && u.getUnity && typeof u.getUnity === 'function'){u.getUnity().SendMessage('SpilGamesAPI', 'APILoadingError', '');}};function initUnity () {if(typeof u !== 'undefined' && u.getUnity && typeof u.getUnity === 'function'){var setGameAPILogo = function (logoUrl) {u.getUnity().SendMessage('SpilGamesAPI', 'setGameAPILogo', logoUrl);};var apiInstance;GameAPI.loadAPI(function(api){apiInstance = api;apiInstance.Branding.displaySplashScreen(function(){});var logoData = apiInstance.Branding.getLogo();if(logoData.image){setGameAPILogo(logoData.image);}}, {id:'" + GAME_ID + "'});}};function loadScript(src, callback) {var s,r,t;r = false;s = document.createElement('script');s.type = 'text/javascript';s.src = src;s.onerror = onLoadError;s.onload = s.onreadystatechange = function() {if ( !r && (!this.readyState || this.readyState == 'complete') ){r = true;callback();}};t = document.getElementsByTagName('script')[0];t.parentNode.insertBefore(s, t);};if (typeof GameAPI !== 'undefined') {initUnity();} else {loadScript('http://cdn.gameplayer.io/api/js/game.js', initUnity);}})();"
 			);
 		debugText.text = "APILoaded";
+		Application.ExternalEval ("if (console && console.log) console.log('plugin version : " + version + "');");
 	}
 
 	//this will fire if the API is unable to load for any reason
@@ -124,6 +128,18 @@ public class SpilGamesAPI : MonoBehaviour
 		Application.ExternalEval (
 			"GameAPI.Friends.getFriends(function (friendsData){if(typeof u !== 'undefined' && u.getUnity && typeof u.getUnity === 'function'){u.getUnity().SendMessage('SpilGamesAPI', 'SetUpFriends', JSON.stringify(friendsData));}});"
 		);
+	}
+
+	//submit a player score to the portal
+	public void ScoreSubmit(int score){
+		Application.ExternalEval ("GameAPI.Score.submit('" + score.ToString() + "');"
+		);
+	}
+
+	//submit an award to the portal (dont forget that this needs to be setup in the portal first)
+	public void AwardSubmit(string awardName){
+		Application.ExternalEval ( "GameAPI.Award.submit({award:'" + awardName + "'});"
+			);
 	}
 
 	//Enter the code here to pause your game before an AD, make sure the sound is muted too
